a754bc765b2c55f70529bf46f8718d96e71c273e
sunny2883 <sunnybhushan9185@gmail.com>
2024-03-09 12:00:50 +0530

    Adds aws region

--- instance.tf
+++ instance.tf
@@ -1,22 +1,22 @@
 
 provider "aws" {
-    access_key = "${var.AWS_SECRET_KEY_ACCESS}"
-    secret_key = "${var.AWS_SECRET_KEY_ID}"
-    
+  access_key = var.AWS_SECRET_KEY_ACCESS
+  secret_key = var.AWS_SECRET_KEY_ID
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "git_instance" {
-    ami = var.ami_id
-    count = var.number_of_instances
-    instance_type = var.instance_type
-    key_name = "${aws_key_pair.key-tf.key_name}"
-    associate_public_ip_address = true
+  ami                         = var.ami_id
+  count                       = var.number_of_instances
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.key-tf.key_name
+  associate_public_ip_address = true
 
-      tags = {
-        Name=var.instance_name
-    }
+  tags = {
+    Name = var.instance_name
+  }
 
-      user_data = <<EOF
+  user_data = <<EOF
 #!bin/bash
 yum -y update
 yum install -y docker
--- varaible.tf
+++ varaible.tf
@@ -1,52 +1,52 @@
- variable "AWS_SECRET_KEY_ACCESS" {
-        description = "Access key to AWS conso2323le"
+variable "AWS_SECRET_KEY_ACCESS" {
+  description = "Access key to AWS conso2323le"
 }
 variable "AWS_SECRET_KEY_ID" {
-        description = "Secret key to AWS console"
+  description = "Secret key to AWS console"
 }
 
 
 variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "InstanceForCICD"
+  description = "Name of the instance to be created"
+  default     = "InstanceForCICD"
 }
 
 variable "instance_type" {
-        default = "t2.micro"
+  default = "t2.micro"
 }
 
 
 variable "ami_id" {
-        description = "The AMI to use"
-       
+  description = "The AMI to use"
+
 }
 
 variable "number_of_instances" {
-        description = "number of instances to be created"
-        default = 1
+  description = "number of instances to be created"
+  default     = 1
 }
 
 
 variable "ami_key_pair_name" {
-        default = "my-key"
-        type = string
+  default = "my-key"
+  type    = string
 }
 
 
 variable "security_group_name" {
   description = "name of the srcurity group"
-  type = string
-  
+  type        = string
+
 }
 
 variable "VPC_id" {
   description = "VPC_id"
-  type = string
-  
+  type        = string
+
 }
 
 
 variable "key-value" {
-   description = "key-value for authentication"
-   type = string
+  description = "key-value for authentication"
+  type        = string
 }
